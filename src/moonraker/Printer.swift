fc3414f57a4737e36f1f417d4a2434b6ffcfb67d
Charles Pickering <me@charlespick.xyz>
2022-09-08 17:05:18 -0700

    refactor: move live code to the top
    Signed-off-by: Charles Pickering <me@charlespick.xyz>

--- src/moonraker/Printer.swift
+++ src/moonraker/Printer.swift
@@ -8,105 +8,50 @@
 import Foundation
 
 class Printer: Identifiable, ObservableObject, Codable {
-    static func == (lhs: Printer, rhs: Printer) -> Bool {
-        return(lhs === rhs)
-    }
-    
-    struct JsonRPCRequest: Encodable {
-        let jsonrpc = "2.0" //we use a string to override the default json encoding behavior for decimal values.
-        let method: String
-        let id: Int
-    }
-
-    func newJsonRPCRequest(method: String) -> JsonRPCRequest {
-        return JsonRPCRequest(method: method, id: jsonID)
-    }
     
     @Published var name: String
     @Published var id: UUID
     @Published var url: String
+    @Published var isConnected = false
     var wsocket: URLSessionWebSocketTask?
     var jsonID = 50
     
-    enum CodingKeys: CodingKey {
-        case name
-        case url
-        case uuid
+    // Printer Functions
+    func eStop() {
+        let request = newJsonRPCRequest(method: "printer.emergency_stop")
+        sendMoonrakerCommand(request: request)
     }
     
-    required init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        name = try container.decode(String.self, forKey: .name)
-        id = try container.decode(UUID.self, forKey: .uuid)
-        url = try container.decode(String.self, forKey: .url)
-    }
-    
-    func encode(to encoder: Encoder) throws {
-        var container = encoder.container(keyedBy: CodingKeys.self)
-        try container.encode(name, forKey: .name)
-        try container.encode(id, forKey: .uuid)
-        try container.encode(url, forKey: .url)
-    }
-    
-    init(id: UUID = UUID(), name: String, url: String){
-        self.name = name
-        self.url = url
-        self.id = id
-    }
-    
-    struct ModifiedData {
-        var name: String = ""
-        var url: String = ""
-    }
-    
-    var modifiedData: ModifiedData {
-        ModifiedData(name: name, url: url)
-    }
-    
-    func update(from data: ModifiedData) {
-        name = data.name
-        url = data.url
-    }
-    
-    init(data: ModifiedData) {
-        id = UUID()
-        name = data.name
-        url = data.url
-    }
-    
+    // Websocket operations
     func connect(){
         wsocket = URLSession.shared.webSocketTask(with: URL(string: "ws://\( url )/websocket")!)
         wsocket?.resume()
         ping()
         startReceive()
     }
-    
     func sendMoonrakerCommand(request: JsonRPCRequest){
         var payload = Data()
         do {
             payload = try JSONEncoder().encode(request)
         }
         catch {}
-        
         wsocket?.send(.data(payload)) { error in
             if error == nil{
                 print(error as Any)
             }
         }
     }
-    
     func startReceive() {
         wsocket?.receive() { responce in
             print("incoming message")
             print(responce)
             print("end of message")
             
-            //handle incoming message
+            //handle incoming message on a new task /thread if I need to
             
             self.startReceive()
         }
     }
-    
     func ping(){
         Task {
             try await Task.sleep(nanoseconds: 1_000_000_000)
@@ -117,9 +62,60 @@ class Printer: Identifiable, ObservableObject, Codable {
         }
     }
     
-    func eStop() {
-        let request = newJsonRPCRequest(method: "printer.emergency_stop")
-        sendMoonrakerCommand(request: request)
-        
+    // Helpers for: Identifiable
+    static func == (lhs: Printer, rhs: Printer) -> Bool {
+        return(lhs === rhs)
+    }
+    
+    // Helpers for Websocket comms
+    struct JsonRPCRequest: Encodable {
+        let jsonrpc = "2.0" //we use a string to override the default json encoding behavior for decimal values.
+        let method: String
+        let id: Int
+    }
+    func newJsonRPCRequest(method: String) -> JsonRPCRequest {
+        return JsonRPCRequest(method: method, id: jsonID)
+    }
+    
+    // Helpers for persistent storage
+    enum CodingKeys: CodingKey {
+        case name
+        case url
+        case uuid
+    }
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        name = try container.decode(String.self, forKey: .name)
+        id = try container.decode(UUID.self, forKey: .uuid)
+        url = try container.decode(String.self, forKey: .url)
+    }
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(name, forKey: .name)
+        try container.encode(id, forKey: .uuid)
+        try container.encode(url, forKey: .url)
+    }
+    
+    // Helpers for instantiation and modification
+    init(id: UUID = UUID(), name: String, url: String){
+        self.name = name
+        self.url = url
+        self.id = id
+    }
+    struct ModifiedData {
+        var name: String = ""
+        var url: String = ""
+    }
+    var modifiedData: ModifiedData {
+        ModifiedData(name: name, url: url)
+    }
+    func update(from data: ModifiedData) {
+        name = data.name
+        url = data.url
+    }
+    init(data: ModifiedData) {
+        id = UUID()
+        name = data.name
+        url = data.url
     }
 }
