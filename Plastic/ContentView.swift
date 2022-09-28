ccf154b7549e70e60cd314c4b4d70a1717dcba3c
Charles Pickering <me@charlespick.xyz>
2022-09-27 21:01:34 -0700

    a new beginning
    Signed-off-by: Charles Pickering <me@charlespick.xyz>

---
+++ Plastic/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Plastic
+//
+//  Created by Charles Pickering on 9/27/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Plastic/PlasticApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlasticApp.swift
+//  Plastic
+//
+//  Created by Charles Pickering on 9/27/22.
+//
+
+import SwiftUI
+
+@main
+struct PlasticApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
